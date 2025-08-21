67fb540502c6c5a470db3a1bd5b56ab01a1e1c3f
Kushal Gowda C R <147250588+blue67chillout@users.noreply.github.com>
2025-08-21 11:01:57 +0530

    Update peripheral.v

--- src/peripheral.v
+++ src/peripheral.v
@@ -54,7 +54,7 @@ module tqvp_video_controller (
     
     video_controller inst (
         .clk (clk),
-        .rst (rst_n),
+        .reset (rst_n),
         .hsync (hsync),
         .vsync (vsync),
         .display_on(display_on),
